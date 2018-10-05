94fecd15dc6fa9111d7128199b15b24714075661
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-04 22:47:55 -0300

    firebase

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-'use strict';
- 
 const functions = require('firebase-functions');
 const {dialogflow,Permission} = require('actions-on-google');
 const request = require('request');
+const rp = require('request-promise');
  
 const app = dialogflow();
 
@@ -25,12 +24,29 @@ app.intent('CriarLista - location_permission', (conv, params, permissionGranted)
             if (coordinates) {
                 let lat = coordinates.latitude;
                 let long = coordinates.longitude;
-                const API_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=500&types=supermarket&name=mercado&key=${API_KEY}`;
+                // const API_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=500&types=supermarket&name=mercado&key=${API_KEY}`;
+                const API_URL = "https://viacep.com.br/ws/01310200/json/";
 
-                request(API_URL, function(err, response, body) {
-                    let market = response['results'][0]['name'];
-                    return conv.close(`O mercado mais barato é: ${market}`);
-                });
+                // request(API_URL, function(err, response, body) {
+                //     // let market = response['results'][0]['name'];
+                //     return conv.close(`O mercado mais barato é: ${response["bairro"]}`);
+                // });
+
+                var options = {
+                    uri: 'https://viacep.com.br/ws/01310200/json/',
+                    headers: {
+                        'User-Agent': 'Request-Promise'
+                    },
+                    json: true // Automatically parses the JSON string in the response
+                };
+                 
+                rp(options)
+                    .then(function (repos) {
+                        conv.close(`O mercado mais barato é: ${repos["bairro"]}`);
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
                 
             } else {
                 return conv.close('Sorry, I could not figure out where you are.');
@@ -51,5 +67,8 @@ http://dev.virtualearth.net/REST/v1/Locations/av%20paulista%202300?include=query
 http://dev.virtualearth.net/REST/v1/locationrecog/-46.64896,-23.54486?key=AtH19x_lk5injBwyhlvguL9IJDhLU7C1JAFGcBJ1FQNd7HpCFxxKDPY98W6s0vSg&output=json
 
 https://api.mapbox.com/geocoding/v5/mapbox.places/super%20mercado.json?proximity=-46.64896393464835,-23.544864644676096&access_token=pk.eyJ1IjoibWFya2V0aXplYXBwIiwiYSI6ImNqbXY4OWpoMzAweDcza252OXpkNnh4bTYifQ.9-FyCPCayVSOkqjf5y4jPQ
+
+firebase login
+firebase init
 
 */
